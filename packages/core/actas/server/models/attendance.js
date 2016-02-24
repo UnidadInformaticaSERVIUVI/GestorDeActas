56877f990ae49beeda0b480cb8b8e15ca6357b6d
MatiasParedesF <matiasparedes@me.com>
2016-02-24 15:17:06 -0300

    declaración

---
+++ packages/core/actas/server/models/attendance.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+  Schema = mongoose.Schema;
+  
+
+/**
+ * Attendances Schema
+ */
+var AttendanceSchema = new Schema({
+  acta: {
+    type: Schema.ObjectId,
+    ref: 'Acta',
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+    trim: true
+  },
+   appointment: {
+    type: String,
+    required: true,
+    trim: true
+  },
+   note: {
+    type: String,
+    required: true,
+    trim: true
+  }
+});
+
+AttendanceSchema.path('name').validate(function(name) {
+  return !!name;
+}, 'Name cannot be blank');
+
+AttendanceSchema.path('appointment').validate(function(appointment) {
+  return !!appointment;
+}, 'Appointment cannot be blank');
+
+AttendanceSchema.path('note').validate(function(note) {
+  return !!note;
+}, 'Note cannot be blank');
+
+mongoose.model('attendance', AttendanceSchema);
