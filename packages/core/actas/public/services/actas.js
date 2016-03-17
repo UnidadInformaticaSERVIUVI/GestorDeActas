1170e6487c3832e15bd3b33b4f702011cf1aff98
MatiasParedesF <matiasparedes@me.com>
2016-03-17 10:18:13 -0300

    Eliminancion del Alert

--- packages/core/actas/public/services/actas.js
+++ packages/core/actas/public/services/actas.js
@@ -3,7 +3,6 @@
 //Actas service used for actas REST endpoint
 angular.module('mean.actas').factory('Actas', ['$resource',
   function($resource) {
-           alert("DONWLOAD 2");
     return $resource('api/actas/:actaId', { actaId: '@_id'}, { update: { method: 'PUT'} }, {download: {method: 'DOWNLOAD'}} );
   }
 ]);
